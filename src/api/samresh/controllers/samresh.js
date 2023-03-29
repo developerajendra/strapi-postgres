3649aa0eed72c545753cca7e85f4c0f7e05a4442
rajendra <rajendra.prasad@tothenew.com>
2023-03-29 17:22:09 +0530

    init

---
+++ src/api/samresh/controllers/samresh.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * samresh controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::samresh.samresh');
---
+++ src/api/samresh/services/samresh.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * samresh service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::samresh.samresh');
